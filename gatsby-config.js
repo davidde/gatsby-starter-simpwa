f9856821ac353acc6e03082cd2f7c1ac30757b98
DavidDeprost <dadeprost@gmail.com>
2019-08-20 01:09:39 +0200

    Update to gatsby-theme-simpwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,5 +28,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    `gatsby-theme-simpwa`,
   ],
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,19 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import SEO from '../components/seo'
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <div>
+    <SEO title='404: Not found' />
+    <h1>404: Page not found</h1>
+    <p>
+      Oops! The page you are looking for has been removed or relocated.
+    </p>
+    <Link to='/'>
+      <p>Go Back</p>
+    </Link>
+  </div>
 )
 
 export default NotFoundPage
